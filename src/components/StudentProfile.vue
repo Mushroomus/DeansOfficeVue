a446616d846f8426b6dd9f2694dda479e8e77f69
Piotr <Piotr@DESKTOP-OGB5VIU>
2023-10-03 09:50:37 +0200

    Connected backend API to "StudentProfile" view

--- src/components/StudentProfile.vue
+++ src/components/StudentProfile.vue
@@ -36,15 +36,16 @@
 
     <h2 class="mb-3 fw-bold">Bringing Studies</h2>
     <div class="d-flex align-items-center">
-      <select class="form-select" style="width: 30%;">
-        <option>Select</option>
+      <select class="form-select" v-model="selectedSpecializationMajorYearId" style="width: 30%;">
+        <option v-for="item in filteredSpecializationMajorYear" :key="item.id" :value="item.id">
+          {{ item.specialization.name }} - {{ item.specialization.course }} - {{ ' Year ' + item.majorYear.year }}</option>
       </select>
-      <i @click="addNewItem" class="fas fa-plus ml-5"></i>
+      <i @click="addNewSpecializationToStudent()" class="fas fa-plus ml-5"></i>
     </div><br><br>
 
     <div>
       <ul class="list-group">
-        <li v-for="item in specializationMajorYear" :key="item.id" class="list-group-item" style="width: 30%;">
+        <li v-for="item in studentSpecializationMajorYear" :key="item.id" class="list-group-item" style="width: 30%;">
           {{ item.specialization.name }} - {{ item.specialization.course }} - {{ ' Year ' + item.majorYear.year }}
           <i @click="deleteSpecializationMajorYear(item.id)" class="fas fa-trash ml-3"></i>
         </li>
@@ -63,6 +64,9 @@ export default {
   data() {
     return {
       specializationMajorYear: [],
+      filteredSpecializationMajorYear: [],
+      selectedSpecializationMajorYearId: null,
+      studentSpecializationMajorYear: [],
       studentName: 'John',
       newName: '',
       editingName: false,
@@ -73,9 +77,49 @@ export default {
     };
   },
   mounted() {
+    this.fetchStudentData();
     this.fetchStudentSpecializationMajorYear();
   },
   methods: {
+    async fetchStudentData() {
+      const studentId = 1;
+      try {
+        const response = await axios.get(`http://localhost:8082/student/${studentId}`, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.token}`
+          }
+        });
+        this.studentName = response.data.name;
+        this.studentSurname = response.data.surname;
+      } catch (error) {
+        // Handle any errors that occur during the request
+        console.error('Error fetching workers:', error);
+      }
+    },
+    async fetchSpecializationMajorYear() {
+      const studentId = 1;
+      try {
+        const response = await axios.get(`http://localhost:8082/student/${studentId}/specialization-major-year`, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.token}`
+          }
+        });
+        this.specializationMajorYear = response.data;
+
+        this.filteredSpecializationMajorYear = this.specializationMajorYear.filter((specialization) => {
+          return !this.studentSpecializationMajorYear.some((studentSpecialization) => studentSpecialization.id === specialization.id);
+        });
+
+        if (this.filteredSpecializationMajorYear.length > 0) {
+          this.selectedSpecializationMajorYearId = this.filteredSpecializationMajorYear[0].id;
+        }
+      } catch (error) {
+        // Handle any errors that occur during the request
+        console.error('Error fetching workers:', error);
+      }
+    },
     async fetchStudentSpecializationMajorYear() {
       const studentId = 1;
       try {
@@ -85,8 +129,32 @@ export default {
             'Authorization': `Bearer ${this.token}`
           }
         });
-        this.specializationMajorYear = response.data;
-        console.log(response.data);
+        this.studentSpecializationMajorYear = response.data;
+
+        if(this.specializationMajorYear != []) {
+          this.fetchSpecializationMajorYear();
+        }
+      } catch (error) {
+        // Handle any errors that occur during the request
+        console.error('Error fetching workers:', error);
+      }
+    },
+    async updateStudentData() {
+      const studentId = 1;
+
+       var putName = this.studentName;
+       var putSurname = this.studentSurname;
+
+      try {
+        await axios.put(`http://localhost:8082/student/${studentId}`, {
+          name: putName,
+          surname: putSurname
+        }, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.token}`
+          }
+        });
       } catch (error) {
         // Handle any errors that occur during the request
         console.error('Error fetching workers:', error);
@@ -114,7 +182,7 @@ export default {
     saveStudentName() {
       this.studentName = this.newName;
       this.editingName = false;
-      // Optionally, send a request to update the student's name to a server
+      this.updateStudentData();
     },
     cancelEditName() {
       this.editingName = false;
@@ -126,10 +194,28 @@ export default {
     saveStudentSurname() {
       this.studentSurname = this.newSurname;
       this.editingSurname = false;
-      // Optionally, send a request to update the student's name to a server
+      this.updateStudentData();
     },
     cancelEditSurname() {
       this.editingSurname = false;
+    },
+    async addNewSpecializationToStudent() {
+      const studentId = 1;
+      const specializationMajorYearId = this.selectedSpecializationMajorYearId;
+      try {
+        await axios.post(`http://localhost:8082/student/${studentId}/own-major-details`, {
+          specializationMajorYearId
+        }, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.token}`
+          }
+        });
+        this.fetchStudentSpecializationMajorYear();
+      } catch (error) {
+        // Handle any errors that occur during the request
+        console.error('Error fetching workers:', error);
+      }
     }
   }
 };
